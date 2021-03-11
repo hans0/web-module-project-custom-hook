fda8e72c0a654d39cefa54e927b00bc140f78a79
hans0 <kiessler.hans@gmail.com>
2021-03-10 19:19:51 -0500

    Done with MVP

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,5 +1,9 @@
 import useLocalStorage from './useLocalStorage';
 
 const useDarkMode = () => {
-
+  const [darkMode, setDarkMode] = useLocalStorage('dark-mode', false);
+  
+  return [darkMode, setDarkMode];
 }
+
+export default useDarkMode;
